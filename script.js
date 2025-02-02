901b52135bdb69b6aadf4e8e96b51c4b59270a5a
Tiago Amaral <tiagoamaraltic@gmail.com>
2025-02-01 21:22:34 -0300

    Menu improvement

--- script.js
+++ script.js
@@ -1,36 +1,94 @@
+    // Efeito auto digitação
+
 document.addEventListener("DOMContentLoaded", function() {
-    const textElement = document.getElementById("typedText");
-    const cursor = document.getElementById("cursor");
-    const speed = 100; // Velocidade da digitação
-    let i = 0;
-    const text = "Design that connects, code that transforms!"; // O texto a ser digitado
-    let typedText = ""; // Texto digitado até o momento
-  
-    // Função para digitar o texto com efeito
-    function typeWriter() {
-      if (i < text.length) {
-        // Verifica se o caractere é parte de uma palavra "Design" ou "Code"
-        if (text.substring(i, i + 6) === "Design" || text.substring(i, i + 4) === "code") {
-          // Adiciona a classe 'highlighted' para essas palavras
-          typedText += `<span class="highlighted">${text.substring(i, i + (text.substring(i, i + 6) === "Design" ? 6 : 4))}</span>`;
-          i += (text.substring(i, i + 6) === "Design" ? 6 : 4);
-        } else {
-          // Adiciona o texto normalmente
-          typedText += text.charAt(i);
-          i++;
-        }
-  
-        // Atualiza o conteúdo da tag <p> com o texto digitado e o cursor
-        textElement.innerHTML = typedText + '<span id="cursor">|</span>';
-        
-        // Chama a função novamente após o intervalo
-        setTimeout(typeWriter, speed);
+  const textElement = document.getElementById("typedText");
+  const cursor = document.getElementById("cursor");
+  const speed = 70;
+  let i = 0;
+  const text = "Design that connects, code that transforms!";
+  let typedText = "";
+
+  function typeWriter() {
+    if (i < text.length) {
+      if (text.substring(i, i + 6) === "Design" || text.substring(i, i + 4) === "code") {
+        typedText += `<span class="highlighted">${text.substring(i, i + (text.substring(i, i + 6) === "Design" ? 6 : 4))}</span>`;
+        i += (text.substring(i, i + 6) === "Design" ? 6 : 4);
       } else {
-        // Remove o cursor depois de terminar a digitação
-        cursor.style.display = 'none';
+        typedText += text.charAt(i);
+        i++;
       }
+
+      textElement.innerHTML = typedText + '<span id="cursor">|</span>';
+      setTimeout(typeWriter, speed);
+    } else {
+      cursor.style.display = 'none';
     }
+  }
+
+  typeWriter();
+});
+
+    // Menu hamburguer click
+
+    document.addEventListener("DOMContentLoaded", function() {
+      const menuHamburguer = document.querySelector('.menu-hamburguer');
+      const menu = document.querySelector('.menu');
+      const menuItems = document.querySelectorAll('.menu li a');
   
-    typeWriter(); // Inicia o efeito de digitação
+      function toggleMenu() {
+          menu.classList.toggle('active');
+          menuHamburguer.classList.toggle('active');
+      }
+  
+      menuHamburguer.addEventListener('click', function(event) {
+          event.stopPropagation();
+          toggleMenu();
+      });
+  
+      document.addEventListener('click', function(event) {
+          if (!menu.contains(event.target) && !menuHamburguer.contains(event.target)) {
+              menu.classList.remove('active');
+              menuHamburguer.classList.remove('active');
+          }
+      });
+  
+      menuItems.forEach(item => {
+          item.addEventListener('click', function() {
+              menu.classList.remove('active');
+              menuHamburguer.classList.remove('active');
+          });
+      });
+  });
+  
+
+
+
+    // Menu scroll top
+
+    document.addEventListener("DOMContentLoaded", function() {
+      const header = document.querySelector('.bg-menu');
+      const linha = document.querySelector('.linha');
+      let lastScrollTop = 0;
+      let isHeaderVisible = true;
+  
+      window.addEventListener('scroll', function() {
+          let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+  
+          if (currentScroll > lastScrollTop) {
+              if (currentScroll > header.offsetHeight && isHeaderVisible) {
+                  header.classList.add('hidden');
+                  linha.classList.add('hidden');  // Adiciona a classe hidden na linha
+                  isHeaderVisible = false;
+              }
+          } else if (currentScroll < lastScrollTop) {
+              if (!isHeaderVisible) {
+                  header.classList.remove('hidden');
+                  linha.classList.remove('hidden');  // Remove a classe hidden na linha
+                  isHeaderVisible = true;
+              }
+          }
+  
+          lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
+      });
   });
   
